bd21ec2a22399e96c71c2920a7736a0755007098
Yigit Zarbun <yzarbun@gmail.com>
2023-03-17 09:41:36 +0300

    heroku test

--- src/redux-stuff/actions.js
+++ src/redux-stuff/actions.js
@@ -28,7 +28,7 @@ export const GET_FOLLOWS = "GET_FOLLOWS";
 export const ADD_FOLLOW = "ADD_FOLLOW";
 export const DELETE_FOLLOW = "DELETE_FOLLOW";
 
-let productionUrl = "https://teritori.vercel.app/";
+let productionUrl = "https://teritori.herokuapp.com/";
 let developmentUrl = "http://localhost:9000/";
 let url = productionUrl;
 
